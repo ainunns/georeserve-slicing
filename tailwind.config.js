34adf6239d03f511cb40d967d601d86195c01b42
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2023-11-12 19:44:29 +0700

    feat: create page reservations

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html, js}", "./**/*.html"],
+  theme: {
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
+    },
+  },
+};
